eb21d7a950ab9af5de692905f9c5bda945f7007a
Yuhnosuke <h.yuunosuke@gmail.com>
2025-05-13 13:18:29 +0900

    modified variable name

--- design-bucket-sort.py
+++ design-bucket-sort.py
@@ -11,10 +11,11 @@ def bucket_sort(array: List[int]):
     i = 0
 
     # pointer for the array of frequency of numbers
-    for c in range(len(counts)):
+    # n is pointer, at the same time, is number
+    for n in range(len(counts)):
         # insert actual number as long as it appeared to array
-        for _ in range(counts[c]):
-            array[i] = c
+        for _ in range(counts[n]):
+            array[i] = n
             i += 1
 
     return array
