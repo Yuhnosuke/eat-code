0b949187eadc4fb116fdad4e78c9a11834aadce7
Yuhnosuke <h.yuunosuke@gmail.com>
2025-05-20 11:47:01 +0900

    70. Climbing Stairs(bottom up)

--- 0070-climbing-stairs.py
+++ 0070-climbing-stairs.py
@@ -1,3 +1,4 @@
+# top down
 class Solution:
     def climbStairs(self, n: int) -> int:
         def f(n, memo):
@@ -11,3 +12,19 @@ class Solution:
             return memo[n]
 
         return f(n, {})
+
+
+# bottom up
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1]
+
+        return dp[n]
