1c67fc809df0f305711e9e71b6cbe75c8f054e5a
Yuhnosuke <h.yuunosuke@gmail.com>
2025-05-21 15:03:32 +0900

    refactored 53. Maximum Subarray

--- 0053-maximum-subarray.py
+++ 0053-maximum-subarray.py
@@ -5,11 +5,7 @@ class Solution:
         max_sum = nums[0]
 
         for num in nums:
-            if curr_sum < 0:
-                curr_sum = 0
-
-            curr_sum += num
-
+            curr_sum = max(curr_sum + num, num)
             max_sum = max(max_sum, curr_sum)
 
         return max_sum
