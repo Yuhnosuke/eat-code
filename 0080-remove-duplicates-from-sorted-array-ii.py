b072040e1b43125477437e5cdd529205dc586cf7
Yuhnosuke <h.yuunosuke@gmail.com>
2025-05-22 15:51:12 +0900

    80. Remove Duplicates from Sorted Array II

---
+++ 0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # index of the number to be updated
+        j = 2
+
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
