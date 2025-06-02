9f0e37ccdc732f257622c72280333d932831e556
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-02 12:54:32 +0900

    refactor 1584. Min Cost to Connect All Points

--- 1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points.py
@@ -1,3 +1,5 @@
+# this is not efficient because each node is stored as set,
+# which uses extra memory.
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
 
@@ -63,4 +65,49 @@ class Solution:
         min_cost = 0
         for src, dst, cost in mst:
             min_cost += cost
+        return min_cost
+
+
+# node should be denoted as index(int).
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+
+        # build adjacency list
+        # src -> (dst, cost)
+        adj = defaultdict(list)
+        for i in range(len(points)):
+            xi, yi = points[i]
+
+            for j in range(i + 1, len(points)):
+                xj, yj = points[j]
+
+                distance = abs(xi - xj) + abs(yi - yj)
+
+                adj[i].append((j, distance))
+                adj[j].append((i, distance))
+
+        # inilialize min_heap
+        # (cost, node)
+        min_heap = [(0, 0)]
+
+        # initialize visited set
+        visited = set()
+
+        min_cost = 0
+
+        # prim
+        while len(visited) < len(points):
+            cost, dst = heapq.heappop(min_heap)
+
+            if dst in visited:
+                continue
+
+            visited.add(dst)
+
+            min_cost += cost
+
+            for nei_node, nei_cost in adj[dst]:
+                if nei_node not in visited:
+                    heapq.heappush(min_heap, (nei_cost, nei_node))
+
         return min_cost
