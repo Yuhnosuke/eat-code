76d796ee1e85465a666f698e189f963cc7ec9592
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-02 21:22:20 +0900

    1584. Min Cost to Connect All Points(Kruskal's)

--- 1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points.py
@@ -1,3 +1,4 @@
+# Prim's 1
 # this is not efficient because each node is stored as set,
 # which uses extra memory.
 class Solution:
@@ -68,6 +69,7 @@ class Solution:
         return min_cost
 
 
+# Prim's 2
 # node should be denoted as index(int).
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
@@ -109,5 +111,62 @@ class Solution:
             for nei_node, nei_cost in adj[dst]:
                 if nei_node not in visited:
                     heapq.heappush(min_heap, (nei_cost, nei_node))
+
+        return min_cost
+
+
+# Kruskal's
+class UnionFind:
+
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+        self.connected_components = n
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x_root, y_root = self.find(x), self.find(y)
+
+        if x_root == y_root:
+            return False
+
+        if self.rank[x_root] > self.rank[y_root]:
+            self.parent[y_root] = x_root
+        elif self.rank[y_root] > self.rank[x_root]:
+            self.parent[x_root] = y_root
+        else:
+            self.parent[y_root] = x_root
+            self.rank[x_root] += 1
+
+        self.connected_components -= 1
+        return True
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # (cost, v1, v2)
+        min_heap = []
+        for i in range(len(points)):
+            xi, yi = points[i]
+
+            for j in range(i + 1, len(points)):
+                xj, yj = points[j]
+                dist = abs(xi - xj) + abs(yi - yj)
+                heapq.heappush(min_heap, (dist, i, j))
+
+        min_cost = 0
+        uf = UnionFind(len(points))
+
+        while uf.connected_components > 1:
+            cost, i, j = heapq.heappop(min_heap)
+
+            if not uf.union(i, j):
+                continue
+
+            min_cost += cost
 
         return min_cost
