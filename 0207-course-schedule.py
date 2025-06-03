842d6a8409d56717586c2f24df39d1a145495c52
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-03 11:35:58 +0900

    207. Course Schedule(topological sort)

--- 0207-course-schedule.py
+++ 0207-course-schedule.py
@@ -1,3 +1,4 @@
+# dfs
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         course_prerequisite_graph = {}
@@ -29,6 +30,42 @@ class Solution:
 
         for c in range(numCourses):
             if not dfs(c):
+                return False
+
+        return True
+
+
+# topological sort
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {i: [] for i in range(numCourses)}
+        for course, prerequisite in prerequisites:
+            adj[course].append(prerequisite)
+
+        visited = set()
+        path = set()
+        # topological_order = []
+
+        def dfs(course):
+            if course in path:
+                return False
+
+            if course in visited:
+                return True
+
+            path.add(course)
+            visited.add(course)
+
+            for prerequisite in adj[course]:
+                if not dfs(prerequisite):
+                    return False
+
+            # topological_order.append(course)
+            path.remove(course)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
                 return False
 
         return True
