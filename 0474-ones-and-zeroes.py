21b5d5a9cc4f554bea68f37e2c4e1fdc3a68fb23
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-05 08:58:57 +0900

    474. Ones and Zeroes(bottom up)

--- 0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes.py
@@ -27,3 +27,19 @@ class Solution:
 
         memo = {}
         return dp(0, 0, 0, memo)
+
+
+## bottom up
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for ch in strs:
+            zeros = ch.count("0")
+            ones = ch.count("1")
+
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones])
+
+        return dp[m][n]
