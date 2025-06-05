3d72938d7380eaeaf8c220d6ded8d12888b052d6
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-05 18:55:47 +0900

    322. Coin Change

---
+++ 0322-coin-change.py
@@ -0,0 +1,67 @@
+# top down1
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        def dp(i: int, curr_amount: int) -> int:
+            if curr_amount == 0:
+                return 0
+
+            if i == len(coins):
+                return float("inf")
+
+            if curr_amount < 0:
+                return float("inf")
+
+            if (i, curr_amount) in memo:
+                return memo[(i, curr_amount)]
+
+            not_take = dp(i + 1, curr_amount)
+            take = 1 + dp(i, curr_amount - coins[i])
+
+            memo[(i, curr_amount)] = min(not_take, take)
+            return memo[(i, curr_amount)]
+
+        memo = {}
+        ans = dp(0, amount)
+        return ans if ans != float("inf") else -1
+
+
+# top down2
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        def dp(curr_amount: int) -> int:
+            if curr_amount == 0:
+                return 0
+
+            if curr_amount in memo:
+                return memo[curr_amount]
+
+            res = float("inf")
+
+            for coin in coins:
+                if curr_amount - coin >= 0:
+                    res = min(res, 1 + dp(curr_amount - coin))
+
+            memo[curr_amount] = res
+            return memo[curr_amount]
+
+        memo = {}
+        ans = dp(amount)
+        return ans if ans != float("inf") else -1
+
+
+# bottom up
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+
+        ans = dp[-1]
+        return dp[-1] if ans != float("inf") else -1
