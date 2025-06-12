01c821408e2c685b0f4cb67586354047ee4875d2
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-12 09:47:09 +0900

    48. Rotate Image

--- 0048-rotate-image.py
+++ 0048-rotate-image.py
@@ -16,3 +16,37 @@ class Solution:
         for row in range(rows):
             for column in range(columns):
                 matrix[row][column] = rotated[row][column]
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = len(matrix)
+        columns = len(matrix[0])
+
+        # 1. swap vertically
+        top = 0
+        bottom = rows - 1
+
+        while top < bottom:
+            for column in range(columns):
+                tmp = matrix[top][column]
+
+                matrix[top][column] = matrix[bottom][column]
+                matrix[bottom][column] = tmp
+
+            top += 1
+            bottom -= 1
+
+        # 2. swap diagonally
+        for row in range(rows):
+            # start from rows + 1 to prevent from swapping twice
+            for column in range(row + 1, columns):
+                tmp = matrix[row][column]
+
+                matrix[row][column] = matrix[column][row]
+                matrix[column][row] = tmp
+
+        return matrix
