0c386ac62b00a144fae33c5b0bed0a6f5c91219a
Yuhnosuke <h.yuunosuke@gmail.com>
2025-06-13 13:32:48 +0900

    371. Sum of Two Integers

--- 0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers.py
@@ -1,11 +1,19 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
+        # 32bit mask
+        MASK = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
+
         while b != 0:
-            carry = (a & b) >> 1
-            a = a ^ b
+            # limit 32 bit
+            carry = (a & b) & MASK
+            # shift to left by 1 with limiting 32 bit
+            carry = (carry << 1) & MASK
+            a = (a ^ b) & MASK
             b = carry
 
-        return a
-
-
-Solution().getSum(2, 3)
+        if a <= MAX_INT:
+            return a
+        else:
+            # 2 complement
+            return ~(a ^ MASK)
