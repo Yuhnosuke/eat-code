9c4c3e46ed84a273a24636ab9ce54c61f95d74de
Yuhnosuke <h.yuunosuke@gmail.com>
2025-09-10 11:11:29 +0900

    332. Reconstruct Itinerary

---
+++ solution/332-reconstruct-itinerary.py
@@ -0,0 +1,38 @@
+# Time Limit Exceeded
+# O(E ** 2) where E is the number of edges
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+
+        tickets.sort()
+        graph = defaultdict(list)
+
+        for src, dst in tickets:
+            graph[src].append(dst)
+
+        def dfs(src: str) -> bool:
+            if len(ans) == len(tickets) + 1:
+                return True
+
+            if src not in graph:
+                return False
+
+            # not to update for iteration
+            tmp = list(graph[src])
+
+            for i, neighbor in enumerate(tmp):
+                graph[src].pop(i)
+                ans.append(neighbor)
+
+                if dfs(neighbor):
+                    return True
+
+                # if not valid path, then backtrack
+                graph[src].insert(i, neighbor)
+                ans.pop()
+
+            return False
+
+        ans = ["JFK"]
+        dfs("JFK")
+
+        return ans
