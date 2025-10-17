5bb855193f7fec677b32464a1af321249526dc91
Yuhnosuke <h.yuunosuke@gmail.com>
2025-10-17 12:23:59 +0900

    242. Valid Anagram

--- solution/0242-valid-anagram.py
+++ solution/0242-valid-anagram.py
@@ -1,4 +1,35 @@
-from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_ch_to_freq = {}
+
+        for s_ch in s:
+            if s_ch not in s_ch_to_freq:
+                s_ch_to_freq[s_ch] = 0
+            s_ch_to_freq[s_ch] += 1
+
+        for t_ch in t:
+            if t_ch not in s_ch_to_freq:
+                return False
+
+            s_ch_to_freq[t_ch] -= 1
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_ch_to_freq = {}
+
+        for s_ch in s:
+            if s_ch not in s_ch_to_freq:
+                s_ch_to_freq[s_ch] = 0
+            s_ch_to_freq[s_ch] += 1
+
+        for t_ch in t:
+            if t_ch not in s_ch_to_freq or s_ch_to_freq[t_ch] == 0:
+                return False
+            s_ch_to_freq[t_ch] -= 1
+
+        return True
+        return not any(s_ch_to_freq.values())
 
 
 class Solution:
