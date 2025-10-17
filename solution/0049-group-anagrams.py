53e7e99b5ca8bcf349ec2cde9643085c7b7aed37
Yuhnosuke <h.yuunosuke@gmail.com>
2025-10-17 12:51:41 +0900

    49. Group Anagrams

--- solution/0049-group-anagrams.py
+++ solution/0049-group-anagrams.py
@@ -1,6 +1,3 @@
-from collections import defaultdict
-
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # key: sorted str, value: original strs
@@ -16,3 +13,13 @@ class Solution:
             ans.append(grouped_strs)
 
         return ans
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_to_strs = {"".join(sorted(word)): [] for word in strs}
+
+        for word in strs:
+            sorted_to_strs["".join(sorted(word))].append(word)
+
+        return list(sorted_to_strs.values())
