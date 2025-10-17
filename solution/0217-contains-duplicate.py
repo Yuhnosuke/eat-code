20ea11cc271b960b6a1745f2ed50112a6faabd52
Yuhnosuke <h.yuunosuke@gmail.com>
2025-10-17 12:11:36 +0900

    217. Contains Duplicate

--- solution/0217-contains-duplicate.py
+++ solution/0217-contains-duplicate.py
@@ -7,3 +7,19 @@ class Solution:
             freq[num] += 1
 
         return not all([value == 1 for value in freq.values()])
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+
+        return False
