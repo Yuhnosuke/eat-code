5ef3edf1ed4e4b0d827078c70bc82679a624f201
Yuhnosuke <h.yuunosuke@gmail.com>
2025-10-17 14:04:51 +0900

    347. Top K Frequent Elements

--- solution/0347-top-k-frequent-elements.py
+++ solution/0347-top-k-frequent-elements.py
@@ -1,3 +1,4 @@
+# min_heap
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         num_to_freq = defaultdict(int)
@@ -34,3 +35,19 @@ class Solution:
             k -= 1
 
         return res
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        num_to_freq = {}
+        for num in nums:
+            if num not in num_to_freq:
+                num_to_freq[num] = 0
+            num_to_freq[num] += 1
+
+        return list(
+            map(
+                lambda x: x[0],
+                sorted(num_to_freq.items(), reverse=True, key=lambda x: x[1])[:k],
+            )
+        )
