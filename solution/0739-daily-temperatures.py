dab1a53dff8f501710cedd38baf3d8ee9965fa03
Yuhnosuke <h.yuunosuke@gmail.com>
2025-10-20 13:22:33 +0900

    739. Daily Temperatures

--- solution/0739-daily-temperatures.py
+++ solution/0739-daily-temperatures.py
@@ -1,15 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        past_day_stack = []
+        past_days = []
         ans = [0] * len(temperatures)
 
-        for curr_day in range(len(temperatures)):
-            curr_temp = temperatures[curr_day]
-
-            while past_day_stack and curr_temp > temperatures[past_day_stack[-1]]:
-                past_day = past_day_stack.pop()
-                ans[past_day] = curr_day - past_day
-
-            past_day_stack.append(curr_day)
-
+        for t in range(len(temperatures)):
+            while past_days and temperatures[past_days[-1]] < temperatures[t]:
+                p = past_days.pop()
+                ans[p] = t - p
+            past_days.append(t)
         return ans
