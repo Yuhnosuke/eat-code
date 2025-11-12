26e15df86aa6b758fe454235c3f7de119bdd92d0
Yuhnosuke <h.yuunosuke@gmail.com>
2025-11-12 12:38:58 +0900

    3542. Minimum Operations to Convert All Elements to Zero

---
+++ solution/3452-minimum-operations-to-convert-all-elements-to-zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        stack = []
+        ans = 0
+        for num in nums:
+            while stack and stack[-1] > num:
+                stack.pop()
+            if num == 0:
+                continue
+            if not stack or stack[-1] < num:
+                ans += 1
+                stack.append(num)
+        return ans
