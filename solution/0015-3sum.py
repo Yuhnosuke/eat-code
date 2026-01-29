eb309da19f491fa7324381b08c01688e53ee0328
Yuhnosuk Hashimoto <h.yuunosuke@gmail.com>
2026-01-29 07:32:00 -0800

    15. 3Sum

--- solution/0015-3sum.py
+++ solution/0015-3sum.py
@@ -1,31 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         nums.sort()
-        res = []
 
         for i in range(len(nums)):
-            # avoid duplication
+            if nums[i] > 0:
+                break
+
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            finding_sum = 0 - nums[i]
-
-            l = i + 1
-            r = len(nums) - 1
-
+            l, r = i + 1, len(nums) - 1
             while l < r:
                 two_sum = nums[l] + nums[r]
-
-                if two_sum < finding_sum:
+                if two_sum == -nums[i]:
+                    ans.append([nums[i], nums[l], nums[r]])
                     l += 1
-                elif two_sum > finding_sum:
-                    r -= 1
-                else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    # avoid duplication
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
+                elif two_sum > -nums[i]:
+                    r -= 1
+                else:
+                    l += 1
 
-        return res
+        return ans
